f30bcd9e06712f4819e925a3e45113952c6ab691
inessa312 <inessa@usa.com>
2022-01-16 15:41:13 -0600

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Inessa  on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
